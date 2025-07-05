coracle-social/flotilla

Factor space recent activity into its own component

src/routes/spaces/[relay]/+page.svelte
@@ -11,24 +11,17 @@
   import ProfileLink from "@app/components/ProfileLink.svelte"
   import MenuSpaceButton from "@app/components/MenuSpaceButton.svelte"
   import ProfileLatest from "@app/components/ProfileLatest.svelte"
-  import ChannelName from "@app/components/ChannelName.svelte"
   import SpaceJoin from "@app/components/SpaceJoin.svelte"
   import RelayName from "@app/components/RelayName.svelte"
   import RelayDescription from "@app/components/RelayDescription.svelte"
   import SpaceQuickLinks from "@app/components/SpaceQuickLinks.svelte"
-  import {
-    decodeRelay,
-    makeChannelId,
-    channelsById,
-    deriveUserRooms,
-    userRoomsByUrl,
-  } from "@app/state"
+  import SpaceRecentActivity from "@app/components/SpaceRecentActivity.svelte"
+  import {decodeRelay, userRoomsByUrl} from "@app/state"
   import {makeChatPath} from "@app/routes"
   import {pushModal} from "@app/modal"
 
   const url = decodeRelay($page.params.relay)
   const relay = deriveRelay(url)
-  const userRooms = deriveUserRooms(url)
   const joinSpace = () => pushModal(SpaceJoin, {url})
 
   const owner = $derived($relay?.profile?.pubkey)
@@ -88,34 +81,7 @@
   <SpaceQuickLinks {url} />
   <div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
     <div class="flex flex-col gap-2">
-      <div class="card2 bg-alt">
-        <h3 class="mb-4 flex items-center gap-2 text-lg font-semibold">
-          <Icon icon="chat-round" />
-          Recent Activity
-        </h3>
-        <div class="flex flex-col gap-3">
-          {#if $userRooms.length > 0}
-            {#each $userRooms.slice(0, 3) as room (room)}
-              {@const channel = $channelsById.get(makeChannelId(url, room))}
-              <div class="flex items-center gap-3 rounded bg-base-100">
-                <div class="flex items-center gap-2">
-                  {#if channel?.closed || channel?.private}
-                    <Icon icon="lock" size={4} />
-                  {:else}
-                    <Icon icon="hashtag" />
-                  {/if}
-                  <span class="font-medium">
-                    <ChannelName {url} {room} />
-                  </span>
-                </div>
-                <span class="ml-auto text-sm opacity-60">Active conversations</span>
-              </div>
-            {/each}
-          {:else}
-            <p class="text-sm opacity-60">No recent activity</p>
-          {/if}
-        </div>
-      </div>
+      <SpaceRecentActivity {url} />
     </div>
     <div class="flex flex-col gap-2">
       <div class="card2 bg-alt">

Pleb5/flotilla-budabit

src/routes/spaces/[relay]/+page.svelte
@@ -11,18 +11,12 @@
   import ProfileLink from "@app/components/ProfileLink.svelte"
   import MenuSpaceButton from "@app/components/MenuSpaceButton.svelte"
   import ProfileLatest from "@app/components/ProfileLatest.svelte"
-  import ChannelName from "@app/components/ChannelName.svelte"
   import SpaceJoin from "@app/components/SpaceJoin.svelte"
   import RelayName from "@app/components/RelayName.svelte"
   import RelayDescription from "@app/components/RelayDescription.svelte"
   import SpaceQuickLinks from "@app/components/SpaceQuickLinks.svelte"
-  import {
-    decodeRelay,
-    makeChannelId,
-    channelsById,
-    deriveUserRooms,
-    userRoomsByUrl,
-  } from "@app/state"
+  import SpaceRecentActivity from "@app/components/SpaceRecentActivity.svelte"
+  import {decodeRelay, userRoomsByUrl} from "@app/state"
   import {
     makeChatPath,
     makeThreadPath,
@@ -35,7 +29,6 @@
 
   const url = decodeRelay($page.params.relay)
   const relay = deriveRelay(url)
-  const userRooms = deriveUserRooms(url)
   const otherRooms = deriveOtherRooms(url)
   const threadsPath = makeThreadPath(url)
   const calendarPath = makeCalendarPath(url)
@@ -131,72 +124,7 @@
     </div>
     <RelayDescription {url} />
   </div>
-  <div class="card2 bg-alt md:hidden">
-    <h3 class="mb-4 flex items-center gap-2 text-lg font-semibold">
-      <Icon icon="compass-big" />
-      Quick Links
-    </h3>
-    <div class="flex flex-col gap-2">
-      <Link href={threadsPath} class="btn btn-primary w-full justify-start">
-        <div class="relative flex items-center gap-2">
-          <Icon icon="notes-minimalistic" />
-          Threads
-          {#if $notifications.has(threadsPath)}
-            <div
-              class="absolute -right-3 -top-1 h-2 w-2 rounded-full bg-primary-content"
-              transition:fade>
-            </div>
-          {/if}
-        </div>
-      </Link>
-      <Link href={calendarPath} class="btn btn-secondary w-full justify-start">
-        <div class="relative flex items-center gap-2">
-          <Icon icon="calendar-minimalistic" />
-          Calendar
-          {#if $notifications.has(calendarPath)}
-            <div
-              class="absolute -right-3 -top-1 h-2 w-2 rounded-full bg-primary-content"
-              transition:fade>
-            </div>
-          {/if}
-        </div>
-      </Link>
-      {#if $userRooms.length + $otherRooms.length > 10}
-        <label class="input input-sm input-bordered flex flex-grow items-center gap-2">
-          <Icon icon="magnifer" size={4} />
-          <input
-            bind:value={roomSearchQuery}
-            class="grow"
-            type="text"
-            placeholder="Search rooms..." />
-        </label>
-      {/if}
-      {#each filteredRooms() as room (room)}
-        {@const roomPath = makeRoomPath(url, room)}
-        {@const channel = $channelsById.get(makeChannelId(url, room))}
-        <Link href={roomPath} class="btn btn-neutral btn-sm relative w-full justify-start">
-          <div class="flex min-w-0 items-center gap-2 overflow-hidden text-nowrap">
-            {#if channel?.closed || channel?.private}
-              <Icon icon="lock" size={4} />
-            {:else}
-              <Icon icon="hashtag" />
-            {/if}
-            <ChannelName {url} {room} />
-          </div>
-          {#if $notifications.has(roomPath)}
-            <div class="absolute right-1 top-1 h-2 w-2 rounded-full bg-primary" transition:fade>
-            </div>
-          {/if}
-        </Link>
-      {/each}
-      {#if hasNip29($relay)}
-        <Button onclick={addRoom} class="btn btn-neutral btn-sm w-full justify-start">
-          <Icon icon="add-circle" />
-          Create Room
-        </Button>
-      {/if}
-    </div>
-  </div>
+  <SpaceQuickLinks {url} />
   <div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
     <div class="flex flex-col gap-2">
       <div class="card2 bg-alt">