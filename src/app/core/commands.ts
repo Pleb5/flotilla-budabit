2bbdcb118f7f39bfccaf087c9795999b18e09baf
Jon Staab <shtaab@gmail.com>
2025-10-06 17:01:25 -0700

    Re-work storage to optimize file access

--- src/app/core/commands.ts
+++ src/app/core/commands.ts
@@ -110,11 +110,7 @@ import {
 } from "@app/core/state"
 import {loadAlertStatuses} from "@app/core/requests"
 import {platform, platformName, getPushInfo} from "@app/util/push"
-import {preferencesStorageProvider, collectionStorageProvider} from "@src/lib/storage"
-import {extensionSettings} from "@app/extensions/settings"
-import {extensionRegistry} from "@app/extensions/registry"
-import {request} from "@welshman/net"
-import type {ExtensionManifest} from "@app/extensions/types"
+import {preferencesStorageProvider, Collection} from "@src/lib/storage"
 
 // Utils
 
@@ -258,7 +254,7 @@ export const logout = async () => {
   localStorage.clear()
 
   await preferencesStorageProvider.clear()
-  await collectionStorageProvider.clear()
+  await Collection.clearAll()
 }
 
 // Synchronization
