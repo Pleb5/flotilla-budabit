764795bf0abaac46ed93b03f1015163d79882e9e
Jon Staab <shtaab@gmail.com>
2025-10-17 10:19:21 -0700

    Update to new version of welshman, including new thunks and wrap manager

--- src/app/core/commands.ts
+++ src/app/core/commands.ts
@@ -90,6 +90,7 @@ import {
   waitForThunkError,
   getPubkeyRelays,
   userBlossomServers,
+  shouldUnwrap,
 } from "@welshman/app"
 import {compressFile} from "@src/lib/html"
 import type {SettingsValues, Alert} from "@app/core/state"
@@ -103,8 +104,6 @@ import {
   DEFAULT_BLOSSOM_SERVERS,
   userRoomsByUrl,
   userSettingsValues,
-  canDecrypt,
-  ensureUnwrapped,
   userInboxRelays,
   getMembershipUrls,
 } from "@app/core/state"
@@ -691,8 +690,8 @@ export const createAlert = async (params: CreateAlertParams): Promise<CreateAler
 }
 
 export const createDmAlert = async () => {
-  if (!get(canDecrypt)) {
-    enableGiftWraps()
+  if (!shouldUnwrap.get()) {
+    shouldUnwrap.set(true)
   }
 
   return createAlert({
@@ -753,16 +752,6 @@ export const payInvoice = async (invoice: string) => {
     return getWebLn()
       .enable()
       .then(() => getWebLn().sendPayment(invoice))
-  }
-}
-
-// Gift Wraps
-
-export const enableGiftWraps = () => {
-  canDecrypt.set(true)
-
-  for (const event of repository.query([{kinds: [WRAP]}])) {
-    ensureUnwrapped(event)
   }
 }
 
