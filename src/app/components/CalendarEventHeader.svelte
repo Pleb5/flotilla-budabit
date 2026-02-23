3f54a8d4a32ca8c6720a4e786738642063a7839b
five <co22rkn@protonmail.com>
2026-02-23 10:40:55 +0100

    fix calendar card date/time layout

--- src/app/components/CalendarEventHeader.svelte
+++ src/app/components/CalendarEventHeader.svelte
@@ -19,18 +19,26 @@
   const end = $derived(parseInt(meta.end))
 </script>
 
-<div class="flex flex-grow flex-wrap justify-between gap-2">
+<div class="flex flex-col gap-2">
   <p class="text-xl">{meta.title || meta.name}</p>
   {#if !isNaN(start) && !isNaN(end)}
     {@const startDateDisplay = formatTimestampAsDate(start)}
     {@const endDateDisplay = formatTimestampAsDate(end)}
     {@const isSingleDay = startDateDisplay === endDateDisplay}
-    <div class="flex items-center gap-2 text-sm">
-      <Icon icon={ClockCircle} size={4} />
-      <span class="hidden sm:block">{formatTimestampAsDate(start)}</span>
-      {formatTimestampAsTime(start)} — {isSingleDay
-        ? formatTimestampAsTime(end)
-        : formatTimestamp(end)}
+    <div class="flex flex-col items-start gap-1 text-sm">
+      <span>
+        {#if isSingleDay}
+          {startDateDisplay}
+        {:else}
+          {startDateDisplay} — {endDateDisplay}
+        {/if}
+      </span>
+      <div class="flex items-center gap-2">
+        <Icon icon={ClockCircle} size={4} />
+        {formatTimestampAsTime(start)} — {isSingleDay
+          ? formatTimestampAsTime(end)
+          : formatTimestamp(end)}
+      </div>
     </div>
   {/if}
 </div>
